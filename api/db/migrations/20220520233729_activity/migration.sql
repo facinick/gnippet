e19288fe04ea634e5d4b07f5ac3abf2369dc5be8
shriyans kapoor <shriyanskapoor@shriyanss-MBP.Dlink>
2022-05-25 05:36:18 +0530

    cleared migrations

--- api/db/migrations/20220520233729_activity/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Comment" ADD COLUMN     "activity" INTEGER NOT NULL DEFAULT 0;
-
--- AlterTable
-ALTER TABLE "Snippet" ADD COLUMN     "activity" INTEGER NOT NULL DEFAULT 0;
