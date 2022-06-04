caaa18f69f2118eaa7ea53886ef73e88de8143e5
shriyans kapoor <shriyanskapoor@shriyanss-MBP.Dlink>
2022-06-04 08:22:01 +0530

    prod db migrations cleanup

--- api/db/migrations/20220531083710_bookmark_schema_edit/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `value` on the `Bookmark` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Bookmark" DROP COLUMN "value";
